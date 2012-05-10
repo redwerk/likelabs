a91f295a9e015cc41434408d8139803603d0c801
limitman <limitman@5beb62dc-ff0c-0410-899a-bd8837d9df5a>
2012-05-10 16:10:23 +0000

    Research: implement prototype of SMS sending application
    git-svn-id: svn+ssh://redwerk.com/home/svn/likelabs@4956 5beb62dc-ff0c-0410-899a-bd8837d9df5a

--- server/infrastructure/src/main/java/com/redwerk/likelabs/infrastructure/messaging/SmsServiceImpl.java
+++ server/infrastructure/src/main/java/com/redwerk/likelabs/infrastructure/messaging/SmsServiceImpl.java
@@ -1,34 +1,36 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.redwerk.likelabs.infrastructure.messaging;
 
 
-import com.redwerk.likelabs.application.impl.registration.PasswordGenerator;
 import com.redwerk.likelabs.application.messaging.SmsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import java.net.URLEncoder;
 import java.text.MessageFormat;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SmsServiceImpl implements SmsService{
 
-    private static final String SMS_URL_SENDER_TEMPLATE = "http://sms2.cdyne.com/sms.svc/SimpleSMSsend?PhoneNumber=011{0}&Message={1}";
+    @Value("#{applicationProperties['sms.service.url']}")
+    private String SMS_URL_SENDER_TEMPLATE;
 
+    @Value("#{applicationProperties['sms.service.username']}")
+    private String username;
+
+    @Value("#{applicationProperties['sms.service.password']}")
+    private String password;
+
+    // = "http://sms2.cdyne.com/sms.svc/SimpleSMSsend?PhoneNumber=011{0}&Message={1}";
+    
     @Override
     public boolean sendMessage(String recipientPhone, String text) {
-        
-        try {
 
+        try {
+            recipientPhone = recipientPhone.replaceAll("[^\\d]*", "");
             HttpClient httpClient = new HttpClient();
-            String url = MessageFormat.format(SMS_URL_SENDER_TEMPLATE, recipientPhone.substring(1), URLEncoder.encode(text, "UTF-8"));
+            String url = MessageFormat.format(SMS_URL_SENDER_TEMPLATE, username, password, recipientPhone, URLEncoder.encode(text, "UTF-8"));
             HttpMethod method = new GetMethod(url);
             httpClient.executeMethod(method);
             method.releaseConnection();
