931df1421b03ad6e1f369f35742bcb73e61aae6a
volodya <volodya@5beb62dc-ff0c-0410-899a-bd8837d9df5a>
2012-05-29 16:05:36 +0000

    Real tablet id should be used
    git-svn-id: svn+ssh://redwerk.com/home/svn/likelabs@5098 5beb62dc-ff0c-0410-899a-bd8837d9df5a

--- client/ios/LikeLabs/services/ReviewService.m
+++ client/ios/LikeLabs/services/ReviewService.m
@@ -5,14 +5,11 @@
 
 @implementation ReviewService
 
-static NSUInteger const DEFAULT_TABLET_ID = 1;
-
 - (void)postReview:(Review *)review {
-    //TODO: turn off debug in ASIHTTPRequestConfig.h
-    ASIHTTPRequest *request = [[ASIHTTPRequest alloc] initWithURL:[URLUtil getReviewUrlForTablet:DEFAULT_TABLET_ID]];
+    SettingsDao *dao = [[SettingsDao alloc] init];
+    ASIHTTPRequest *request = [[ASIHTTPRequest alloc] initWithURL:[URLUtil getReviewUrlForTablet:dao.tabletId]];
     [request addRequestHeader:CONTENT_TYPE_KEY value:CONTENT_TYPE_VALUE];
 
-    SettingsDao *dao = [[SettingsDao alloc] init];
     [request addRequestHeader:TABLET_API_KEY value:dao.apiKey];
     [dao release];
     
