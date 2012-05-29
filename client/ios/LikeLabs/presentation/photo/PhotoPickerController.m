4f188ef0cc3786ff588ff84fe2f82840721cf422
volodya <volodya@5beb62dc-ff0c-0410-899a-bd8837d9df5a>
2012-05-29 19:31:04 +0000

    Image rotation according to device orientation during the image capture was added
    git-svn-id: svn+ssh://redwerk.com/home/svn/likelabs@5111 5beb62dc-ff0c-0410-899a-bd8837d9df5a

--- client/ios/LikeLabs/presentation/photo/PhotoPickerController.m
+++ client/ios/LikeLabs/presentation/photo/PhotoPickerController.m
@@ -10,12 +10,13 @@
 @property (assign) NSInteger seconds;
 - (void) initCapture;
 - (void) captureImage;
+- (UIImage *)scaleAndRotateImage:(UIImage *)image;
 @end
 
 @implementation PhotoPickerController
 
 static const float PADDING = 20;
-static const int COUNTER_LENGTH = 1;
+static const int COUNTER_LENGTH = 2;
 static const float TIMER_INTERVAL = 0.2;
 NSString *const kImageCapturedSuccessfully = @"ImageCapturedSuccessfully";
 
@@ -77,13 +78,13 @@ NSString *const kImageCapturedSuccessfully = @"ImageCapturedSuccessfully";
 }
 
 - (void)dealloc {
-    self.image = nil;
+//    self.image = nil;
     [_captureSession release];
     [_previewLayer release];
     [_img release];
     [_label release];
     [_imageOutput release];
-    [_image release];
+//    [_image release];
     [super dealloc];
 }
 
@@ -172,20 +173,123 @@ NSString *const kImageCapturedSuccessfully = @"ImageCapturedSuccessfully";
             break; 
         }
 	}
-    videoConnection.videoOrientation = [UIApplication sharedApplication].statusBarOrientation;
 	[self.imageOutput captureStillImageAsynchronouslyFromConnection:videoConnection 
                                                          completionHandler:^(CMSampleBufferRef imageSampleBuffer, NSError *error) { 
-//                                                             CFDictionaryRef exifAttachments = CMGetAttachment(imageSampleBuffer, kCGImagePropertyExifDictionary, NULL);
-//                                                             if (exifAttachments) {
-//                                                                 NSLog(@"attachements: %@", exifAttachments);
-//                                                             } else { 
-//                                                                 NSLog(@"no attachments");
-//                                                             }
                                                              NSData *imageData = [AVCaptureStillImageOutput jpegStillImageNSDataRepresentation:imageSampleBuffer];    
-                                                             _image = [[UIImage alloc] initWithData:imageData];
+                                                             UIImageOrientation io;
+                                                             switch (self.interfaceOrientation) {
+                                                                 case UIInterfaceOrientationPortrait:
+                                                                     io = UIImageOrientationRight;
+                                                                     break;
+                                                                 case UIInterfaceOrientationLandscapeLeft:
+                                                                     io = UIImageOrientationUp;
+                                                                     break;
+                                                                 case UIInterfaceOrientationLandscapeRight:
+                                                                     io = UIImageOrientationDown;
+                                                                     break;
+                                                                 case UIInterfaceOrientationPortraitUpsideDown:
+                                                                     io = UIImageOrientationLeft;
+                                                                     break;
+                                                             }
+                                                             UIImage* tmp = [UIImage imageWithCGImage:[[UIImage alloc] initWithData:imageData].CGImage scale:1.0 orientation:io];
+                                                             _image = [self scaleAndRotateImage:tmp];
                                                              
                                                              [[NSNotificationCenter defaultCenter] postNotificationName:kImageCapturedSuccessfully object:nil];
                                                          }];
 }
+
+- (UIImage *)scaleAndRotateImage:(UIImage *)image {
+    CGImageRef imgRef = image.CGImage;
+    
+    CGFloat width = CGImageGetWidth(imgRef);
+    CGFloat height = CGImageGetHeight(imgRef);
+    
+    CGAffineTransform transform = CGAffineTransformIdentity;
+    CGRect bounds = CGRectMake(0, 0, width, height);
+    CGFloat scaleRatio = bounds.size.width / width;
+    CGSize imageSize = CGSizeMake(CGImageGetWidth(imgRef), CGImageGetHeight(imgRef));
+    CGFloat boundHeight;
+    UIImageOrientation orient = image.imageOrientation;
+    switch(orient) {
+            
+            case UIImageOrientationUp: //EXIF = 1
+            transform = CGAffineTransformIdentity;
+            break;
+            
+            case UIImageOrientationUpMirrored: //EXIF = 2
+            transform = CGAffineTransformMakeTranslation(imageSize.width, 0.0);
+            transform = CGAffineTransformScale(transform, -1.0, 1.0);
+            break;
+            
+            case UIImageOrientationDown: //EXIF = 3
+            transform = CGAffineTransformMakeTranslation(imageSize.width, imageSize.height);
+            transform = CGAffineTransformRotate(transform, M_PI);
+            break;
+            
+            case UIImageOrientationDownMirrored: //EXIF = 4
+            transform = CGAffineTransformMakeTranslation(0.0, imageSize.height);
+            transform = CGAffineTransformScale(transform, 1.0, -1.0);
+            break;
+            
+            case UIImageOrientationLeftMirrored: //EXIF = 5
+            boundHeight = bounds.size.height;
+            bounds.size.height = bounds.size.width;
+            bounds.size.width = boundHeight;
+            transform = CGAffineTransformMakeTranslation(imageSize.height, imageSize.width);
+            transform = CGAffineTransformScale(transform, -1.0, 1.0);
+            transform = CGAffineTransformRotate(transform, 3.0 * M_PI / 2.0);
+            break;
+            
+            case UIImageOrientationLeft: //EXIF = 6
+            boundHeight = bounds.size.height;
+            bounds.size.height = bounds.size.width;
+            bounds.size.width = boundHeight;
+            transform = CGAffineTransformMakeTranslation(0.0, imageSize.width);
+            transform = CGAffineTransformRotate(transform, 3.0 * M_PI / 2.0);
+            break;
+            
+            case UIImageOrientationRightMirrored: //EXIF = 7
+            boundHeight = bounds.size.height;
+            bounds.size.height = bounds.size.width;
+            bounds.size.width = boundHeight;
+            transform = CGAffineTransformMakeScale(-1.0, 1.0);
+            transform = CGAffineTransformRotate(transform, M_PI / 2.0);
+            break;
+            
+            case UIImageOrientationRight: //EXIF = 8
+            boundHeight = bounds.size.height;
+            bounds.size.height = bounds.size.width;
+            bounds.size.width = boundHeight;
+            transform = CGAffineTransformMakeTranslation(imageSize.height, 0.0);
+            transform = CGAffineTransformRotate(transform, M_PI / 2.0);
+            break;
+            
+            default:
+            [NSException raise:NSInternalInconsistencyException format:@"Invalid image orientation"];
+            
+        }
+    
+    UIGraphicsBeginImageContext(bounds.size);
+    
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    if (orient == UIImageOrientationRight || orient == UIImageOrientationLeft) {
+        CGContextScaleCTM(context, -scaleRatio, scaleRatio);
+        CGContextTranslateCTM(context, -height, 0);
+        }
+    else {
+        CGContextScaleCTM(context, scaleRatio, -scaleRatio);
+        CGContextTranslateCTM(context, 0, -height);
+        }
+    
+    CGContextConcatCTM(context, transform);
+    
+    CGContextDrawImage(UIGraphicsGetCurrentContext(), CGRectMake(0, 0, width, height), imgRef);
+    UIImage *imageCopy = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return imageCopy;
+}
+
 
 @end
