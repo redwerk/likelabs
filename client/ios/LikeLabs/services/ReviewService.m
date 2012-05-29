6eb4e0d580396990fa4ec8aa9874ec5268e29b7f
volodya <volodya@5beb62dc-ff0c-0410-899a-bd8837d9df5a>
2012-05-29 17:32:21 +0000

    The review shouldn't be sent to server if the primary phone is empty
    git-svn-id: svn+ssh://redwerk.com/home/svn/likelabs@5108 5beb62dc-ff0c-0410-899a-bd8837d9df5a

--- client/ios/LikeLabs/services/ReviewService.m
+++ client/ios/LikeLabs/services/ReviewService.m
@@ -6,20 +6,23 @@
 @implementation ReviewService
 
 - (void)postReview:(Review *)review {
-    SettingsDao *dao = [[SettingsDao alloc] init];
-    ASIHTTPRequest *request = [[ASIHTTPRequest alloc] initWithURL:[URLUtil getReviewUrlForTablet:dao.tabletId]];
-    [request addRequestHeader:CONTENT_TYPE_KEY value:CONTENT_TYPE_VALUE];
+    if (review.user.phone && review.user.phone.length) {
+        SettingsDao *dao = [[SettingsDao alloc] init];
+        ASIHTTPRequest *request = [[ASIHTTPRequest alloc] initWithURL:[URLUtil getReviewUrlForTablet:dao.tabletId]];
+        [request addRequestHeader:CONTENT_TYPE_KEY value:CONTENT_TYPE_VALUE];
 
-    [request addRequestHeader:TABLET_API_KEY value:dao.apiKey];
-    [dao release];
-    
-    [request appendPostData:[[review serializeToXml] dataUsingEncoding:NSUTF8StringEncoding]];
-    [request setDelegate:self];
-    [request startAsynchronous];    
+        [request addRequestHeader:TABLET_API_KEY value:dao.apiKey];
+        [dao release];
+        
+        [request appendPostData:[[review serializeToXml] dataUsingEncoding:NSUTF8StringEncoding]];
+        [request setDelegate:self];
+        [request startAsynchronous];
+    }
 }
 
 - (void)requestFinished:(ASIHTTPRequest *)request {
     NSLog(@"Response status: %d", request.responseStatusCode);
+    NSLog(@"Response string: %@", request.responseString);
 }
 
 - (void)requestFailed:(ASIHTTPRequest *)request {
